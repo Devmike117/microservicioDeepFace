c2faa3aca2383806d96b1309662836b03d203eb7
Mike <mikealesso768@gmail.com>
2025-10-21 13:28:50 -0600

    Update server port to 10000 and clean code

--- deepface_service.py
+++ deepface_service.py
@@ -50,4 +50,5 @@ async def generate_embedding(file: UploadFile = File(...)):
             os.remove(file_location)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
+
